7d55ace17175ba875a20208379b6cea08f261a70
Rafael Pilartes <rafaelpilartes.rlps@gmail.com>
2023-08-29 14:51:40 +0100

    ui - mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
+import { useContext, useState } from 'react'
+
 import Dropdown from './Dropdown'
 import { Phone, Mail, Dot, UserCircle2, HeartHandshake } from 'lucide-react'
 import { BaseButton } from './buttons/baseButton'
 import { routsNameMain } from '../data/routsName'
 import { AiOutlineMenuFold, AiOutlineMenuUnfold } from 'react-icons/ai'
 import { CgMenuGridR } from 'react-icons/cg'
+import { ModalMenuMobile } from './modal/ModalMenuMobile'
+import { AppContext } from '../provider/AppProvider'
 
 export const Header = () => {
+  const { menuIsVisible, setMenuIsVisible } = useContext(AppContext)
+
   const dropdownItemsChurch = [
     { label: 'Sobre', link: routsNameMain.about },
     { label: 'Depoimentos', link: routsNameMain.depositions }
@@ -23,115 +29,129 @@ export const Header = () => {
     { label: 'Célula', link: routsNameMain.cell }
   ]
 
+  const openMenu = () => {
+    setMenuIsVisible(!menuIsVisible)
+  }
+
   return (
-    <header className="fixed z-30 w-full p-4 font-semibold">
-      {/* Container */}
-      <div className="bg-white flex items-center justify-between max-w-s-1030:py-1 ">
-        {/* Logo */}
-        <div className="px-4 py-2  ">
-          <img
-            src="/logo.png"
-            alt="Logo CCI"
-            className="w-full max-w-s-xs min-w-[8rem] "
-          />
-        </div>
+    <>
+      <ModalMenuMobile
+        menuIsVisible={menuIsVisible}
+        setMenuIsVisible={setMenuIsVisible}
+      />
 
-        {/* Links */}
-        <div className="w-full flex items-center justify-between flex-col">
-          {/* Top */}
-          <div className="w-full flex items-center justify-between flex-row border-b border-l border-r border-gray-300 max-w-s-1030:border-b-transparent max-w-s-1030:justify-center">
-            {/* Contacts */}
-            <div className="flex items-center justify-between flex-row gap-4 max-w-s-1030:hidden">
-              <div className="customBasePadding gap-2">
-                <Phone size={14} /> (+244) 923 414 621
-              </div>
-              <div className="customBasePadding gap-2">
-                <Mail size={14} /> geral@cci.com
-              </div>
-            </div>
-
-            {/* Links */}
-            <ul className="flex items-center justify-between flex-row gap-4 max-w-s-570:hidden ">
-              <li className="customBasePadding hover:text-primary-200 transition-all duration-200">
-                <a href={routsNameMain.events}>Eventos</a>
-              </li>
-              <li className="customBasePadding hover:text-primary-200 transition-all duration-200 ">
-                <a
-                  href={routsNameMain.cults}
-                  className="relative flex items-center justify-center flex-row gap-1"
-                >
-                  <span className="relative mr-1 flex h-[0.60rem] w-[0.60rem]">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary-400 opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-[0.60rem] w-[0.60rem] bg-primary-200"></span>
-                  </span>
-                  Cultos
-                </a>
-              </li>
-              <li className="customBasePadding hover:text-primary-200 transition-all duration-200">
-                <a href={routsNameMain.ministry}>Ministério</a>
-              </li>
-            </ul>
-
-            {/* Lang */}
-            {/* <div className="customBasePadding">
-              <div className="z-10">
-                <Dropdown title="Linguagem" items={dropdownItemsLang} />
-              </div>
-            </div> */}
+      <header className="fixed z-30 w-full p-4 font-semibold">
+        {/* Container */}
+        <div className="bg-white flex items-center justify-between max-w-s-1030:py-1 ">
+          {/* Logo */}
+          <div className="px-4 py-2  ">
+            <img
+              src="/logo.png"
+              alt="Logo CCI"
+              className="w-full max-w-s-xs min-w-[8rem] "
+            />
           </div>
 
-          {/* Bottom */}
-          <div className="w-full flex items-center justify-between flex-row border-l border-r border-gray-300 max-w-s-1030:hidden ">
-            {/* Links Nav */}
-            <nav>
-              <ul className="flex items-center justify-between flex-row">
+          {/* Links */}
+          <div className="w-full flex items-center justify-between flex-col">
+            {/* Top */}
+            <div className="w-full flex items-center justify-between flex-row border-b border-l border-r border-gray-300 max-w-s-1030:border-b-transparent max-w-s-1030:justify-center">
+              {/* Contacts */}
+              <div className="flex items-center justify-between flex-row gap-4 max-w-s-1030:hidden">
+                <div className="customBasePadding gap-2">
+                  <Phone size={14} /> (+244) 923 414 621
+                </div>
+                <div className="customBasePadding gap-2">
+                  <Mail size={14} /> geral@cci.com
+                </div>
+              </div>
+
+              {/* Links */}
+              <ul className="flex items-center justify-between flex-row gap-4 max-w-s-570:hidden ">
                 <li className="customBasePadding hover:text-primary-200 transition-all duration-200">
-                  <a href={routsNameMain.home}>Inicio</a>
+                  <a href={routsNameMain.events}>Eventos</a>
                 </li>
-                <li className="py-4 flex justify-center items-center ">
-                  <Dropdown title="A igreja" items={dropdownItemsChurch} />
-                </li>
-                <li className="py-4 flex justify-center items-center ">
-                  <Dropdown title="Conteúdo" items={dropdownItemsContain} />
-                </li>
-                <li className="py-4 flex justify-center items-center ">
-                  <Dropdown title="Participe" items={dropdownItemsJoin} />
+                <li className="customBasePadding hover:text-primary-200 transition-all duration-200 ">
+                  <a
+                    href={routsNameMain.cults}
+                    className="relative flex items-center justify-center flex-row gap-1"
+                  >
+                    <span className="relative mr-1 flex h-[0.60rem] w-[0.60rem]">
+                      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary-400 opacity-75"></span>
+                      <span className="relative inline-flex rounded-full h-[0.60rem] w-[0.60rem] bg-primary-200"></span>
+                    </span>
+                    Cultos
+                  </a>
                 </li>
                 <li className="customBasePadding hover:text-primary-200 transition-all duration-200">
-                  <a href={routsNameMain.blog}>Blog</a>
-                </li>
-                <li className="customBasePadding hover:text-primary-200 transition-all duration-200">
-                  <a href={routsNameMain.contacts}>Contactos</a>
+                  <a href={routsNameMain.ministry}>Ministério</a>
                 </li>
               </ul>
-            </nav>
 
-            {/* Buttons Use */}
-            <div className="customBasePadding">
-              <BaseButton
-                icon={UserCircle2}
-                title="Fazer login"
-                styleBtn="ShineLight"
-              />
+              {/* Lang */}
+              {/* <div className="customBasePadding">
+                <div className="z-10">
+                  <Dropdown title="Linguagem" items={dropdownItemsLang} />
+                </div>
+              </div> */}
+            </div>
+
+            {/* Bottom */}
+            <div className="w-full flex items-center justify-between flex-row border-l border-r border-gray-300 max-w-s-1030:hidden ">
+              {/* Links Nav */}
+              <nav>
+                <ul className="flex items-center justify-between flex-row">
+                  <li className="customBasePadding hover:text-primary-200 transition-all duration-200">
+                    <a href={routsNameMain.home}>Inicio</a>
+                  </li>
+                  <li className="py-4 flex justify-center items-center ">
+                    <Dropdown title="A igreja" items={dropdownItemsChurch} />
+                  </li>
+                  <li className="py-4 flex justify-center items-center ">
+                    <Dropdown title="Conteúdo" items={dropdownItemsContain} />
+                  </li>
+                  <li className="py-4 flex justify-center items-center ">
+                    <Dropdown title="Participe" items={dropdownItemsJoin} />
+                  </li>
+                  <li className="customBasePadding hover:text-primary-200 transition-all duration-200">
+                    <a href={routsNameMain.blog}>Blog</a>
+                  </li>
+                  <li className="customBasePadding hover:text-primary-200 transition-all duration-200">
+                    <a href={routsNameMain.contacts}>Contactos</a>
+                  </li>
+                </ul>
+              </nav>
+
+              {/* Buttons Use */}
+              <div className="customBasePadding">
+                <BaseButton
+                  icon={UserCircle2}
+                  title="Fazer login"
+                  styleBtn="ShineLight"
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* Button */}
-        <div className="w-full max-w-[10rem] customBaseMargin  hidden min-w-s-1390:flex">
-          <BaseButton
-            icon={HeartHandshake}
-            title="DOAR AQUI"
-            styleBtn="LeftToRight"
-          />
+          {/* Button */}
+          <div className="w-full max-w-[10rem] customBaseMargin  hidden min-w-s-1390:flex">
+            <BaseButton
+              icon={HeartHandshake}
+              title="DOAR AQUI"
+              styleBtn="LeftToRight"
+            />
+          </div>
+          <div className="w-full max-w-[2rem] items-center justify-center customBaseMargin max-w-s-1030:flex hidden ">
+            <button
+              onClick={openMenu}
+              className="transition-all duration-300 hover:text-primary-200 "
+            >
+              <CgMenuGridR size={38} />
+              {/* <AiOutlineMenuUnfold /> */}
+            </button>
+          </div>
         </div>
-        <div className="w-full max-w-[2rem] items-center justify-center customBaseMargin max-w-s-1030:flex hidden ">
-          <button className="transition-all duration-300 hover:text-primary-200 ">
-            <CgMenuGridR size={38} />
-            {/* <AiOutlineMenuUnfold /> */}
-          </button>
-        </div>
-      </div>
-    </header>
+      </header>
+    </>
   )
 }
